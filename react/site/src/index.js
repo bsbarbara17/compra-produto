adad78caefbbc384ac3dce1a3709121c7f257a0f
Barbara <barbarabs1703@gmail.com.br>
2021-10-08 14:18:51 -0300

    concluído!!

--- react/site/src/index.js
+++ react/site/src/index.js
@@ -2,10 +2,15 @@ import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import './styles.css'
 
 import App from './InfoX/home'
 import DetalheProduto from './InfoX/detalheProduto'
 import Carrinho from './InfoX/carrinho'
+
+import Contador from './InfoX/comunicacaoComps/contador'
+import Pedido from './InfoX/comunicacaoComps/finalizacaoPedido/pedido'
+import ConclusaoPedido from './InfoX/comunicacaoComps/finalizacaoPedido/conclusao';
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
@@ -15,6 +20,10 @@ ReactDOM.render(
                 <Route path="/" exact={true} component={App} />
                 <Route path="/detalhe" component={DetalheProduto} />
                 <Route path="/carrinho" component={Carrinho} />
+
+                <Route path="/infoX/contador" exact={true} component={Contador} />
+                <Route path="/infoX/pedido" component={Pedido} />
+                <Route path="/infoX/conclusao" component={ConclusaoPedido} />
             </Switch>
         </BrowserRouter>
     </StrictMode>,
